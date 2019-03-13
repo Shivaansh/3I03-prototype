423c9d4d1f46ca99139b0bb430e7220a948127ee
Shivaansh Prasann <shivaanshseal@gmail.com>
2019-03-13 15:37:24 -0400

    Added files
    Use generic1.html for all demos (as of this commit)

---
+++ generic1.js
@@ -0,0 +1,60 @@
+alert(" generic1.js connected");
+$("#Header").css("color", "red");
+$("#phrase3").css("color", "red");
+
+var message = "I HATE YOU is hateful! You sure you want to send this?";
+var title = "HateLess Alert";
+
+//turn Header text back to normal
+$("#Header").on("click", function(){
+    if($(this).text === "Screw You!!")
+    {
+        $(this).text("*******!!");
+    }
+    else{
+        $(this).text("Screw You!!");
+    }
+});
+
+//form checking and alert
+$("#target").submit(function(event) {
+
+    confirm("That comment qualifies as hateful content! Do you still want to keep it?");
+    event.preventDefault();
+ });
+
+
+//windows message
+$("#phrase3").on("click", function(){
+        $(this).text("Windows is better than Linux for programming!!");
+});
+
+//Donald trump message
+$("#phrase2").on("click", function(){
+    $(this).text("Donald Trump tweeted something!");
+});
+
+//Hide button
+$("#Hide").on("click", function(){
+    $("#phrase2").text("*******");
+    $("#phrase2").css("color", "red");
+});
+
+//Show button
+$("#Show").on("click", function(){
+    $("#phrase3").text("Windows is better than Linux for programming!!");
+    $("#phrase3").css("color", "black");
+});
+
+
+
+
+
+/////////
+
+
+
+
+
+
+
